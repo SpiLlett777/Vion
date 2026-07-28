8703be0b897671d5a69fb1d4edc695d4d82baf8d
SpiLlett777 <spillettnettbrett@gmail.com>
2026-07-28 12:16:40 +0300

    refactor(auth-service): Extract gRPC options and microservice creation

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,38 +1,15 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-
-import type { AllConfigs } from '@vion/api/contracts';
-import { PROTO_PATHS } from '@vion/api/shared/utils';
-import { join } from 'path';
 
 import { AppModule } from './app/app.module';
+import { createGrpcServer } from './grpc/grpc.server';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
-	const configService = app.get(ConfigService<AllConfigs>);
+	const configService = app.get(ConfigService);
 
-	const host = configService.get('grpc.host', { infer: true });
-	const port = configService.get('grpc.port', { infer: true });
-
-	const url = `${host}:${port}`;
-
-	app.connectMicroservice<MicroserviceOptions>({
-		transport: Transport.GRPC,
-		options: {
-			package: 'auth.v1',
-			protoPath: PROTO_PATHS.AUTH,
-			url: url,
-			loader: {
-				keepCase: false,
-				longs: String,
-				enums: String,
-				defaults: true,
-				oneofs: true,
-			},
-		},
-	});
+	createGrpcServer(app, configService);
 
 	await app.startAllMicroservices();
 
